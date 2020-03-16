c3fdb4ae3aee925c86911f846211013589d83f31
user <user@user.user>
2020-03-16 16:19:27 +0700

    lesson9

--- lesson9.php
+++ lesson9.php
@@ -7,12 +7,20 @@
 
     $bool_2 = $x <= $y;
 
+    echo $bool_1."<br/>".$bool_2."<hr/>";
 
-    echo $bool_1."<br/>".$bool_2;
+    $bool_3 = !($x == $y);
 
-    $bool_3 == !($x = $y);
+    $bool_4 = $x == $y || $z < $y;
 
-    echo "<br/>".$bool_3;
+    $bool_5 = $z != $y && $x < $y;
+
+    $bool_6 = $z != $x ^ $y < $z;
+
+
+    echo "<br/>".$bool_3."<br/>".$bool_4."<br/>".$bool_5."<br/>".$bool_6;
+
+
 
 
 
