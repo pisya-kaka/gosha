3b7f36ae75e1ab6f5ef22042af15634ea0ddd261
user <user@user.user>
2020-03-16 16:55:10 +0700

    lesson10

--- lesson10.php
+++ lesson10.php
@@ -4,7 +4,16 @@
 
     $bool_1 = $string == true;
     echo "$bool_1 = $string == true;";
-    
+
+    $bool_2 = $num == "";
+    echo "<br/>$bool_2 = $num == \"\";<hr/>";
+
+
+    $bool_3 = $string === true;
+    echo "$bool_3 = $string === true;";
+
+    $bool_4 = $num === "";
+    echo "<br/>$bool_4 = $num === \"\";";
 
 
      ?>
