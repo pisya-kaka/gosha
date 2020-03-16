27b994c9bb88958ace6309105000bd3726fe8046
user <user@user.user>
2020-03-16 18:07:09 +0700

    lesson12

--- lesson12.php
+++ lesson12.php
@@ -1,11 +1,12 @@
     <?php
-    $x = 14;
-    
+    $x = 7;
+
     switch ($x) {
         case "hello" : echo "это строковая переменная"; break;
         case 5 : echo "это строковая равна 5"; break;
         case 7  : echo "это строковая  равна 7"; break;
         case 12 : echo "это строковая равна 12"; break;
+        default : echo "эта переменная нам не известна";
     }
 
 
