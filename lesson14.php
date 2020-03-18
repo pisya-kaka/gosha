4bc18d37f10e9fb1ede28a194cf1d115a5f3c34d
user <user@user.user>
2020-03-18 19:59:47 +0700

    lesson14

---
+++ lesson14.php
@@ -0,0 +1,31 @@
+    <?php
+    function prinWords () {
+        echo "hello!";
+    }
+    $x = 12;
+    $y = 35;
+
+    $sum = $x + $y;
+    prinWords();
+    echo $sum ;
+    
+     ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
