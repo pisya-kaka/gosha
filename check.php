6969a7078ee829057d0568bb0160ed3ef3f9e236
user <user@user.user>
2020-03-19 13:23:18 +0700

    check

--- check.php
+++ check.php
@@ -1,7 +1,8 @@
     <?php
-  if($_POST["name"] =="");
-  echo "Введите имя.<a href='/'> исправить </a>";
-
+  if($_POST["name"] =="")
+  echo "Введите имя. <a href='/'> исправить </a>";
+    else
+        header("Location: lesson19.php");
 
 
 
