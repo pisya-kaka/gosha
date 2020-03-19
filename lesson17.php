28852530b53675f3c6f2d98085b32210757702ad
user <user@user.user>
2020-03-19 11:43:08 +0700

    lesson17

--- lesson17.php
+++ lesson17.php
@@ -5,9 +5,11 @@
     echo "Переменная x = $x<br/>";
 
     function test () {
-        $GLOBALS["x"] += 7;
-        $x = 0;
-        echo "Переменная x = $x<br/>";
+        //$GLOBALS["x"] += 7;
+        //$x = 0;
+        global $x;
+            $x += 10;
+        //echo "Переменная x = $x<br/>";
     }
     test ();
     echo "Переменная x = $x<br/>";
