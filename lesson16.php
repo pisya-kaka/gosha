43ae613457e9a28abe341071c6df319bb120d551
user <user@user.user>
2020-03-19 11:13:48 +0700

    lesson16

---
+++ lesson16.php
@@ -0,0 +1,58 @@
+    <?php
+    $array = array (array(12, 4, 46, "string", true), array ("Example"), array (45, "strong"));
+    for ($i = 0; $i < count($array); $i++){
+        for ($j = 0; $j < count($array[$i]); $j++) {
+            echo $array[$i][$j]." ";
+        }
+        echo"<br/>";
+    }
+    ?>
+
+    
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
