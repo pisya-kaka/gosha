2a883ec881cd531c08273528d1a3e7927fbd6cb3
user <user@user.user>
2020-03-19 13:34:01 +0700

    lesson20

---
+++ lesson20.php
@@ -0,0 +1,65 @@
+    <?php
+    $x = 15;
+    if (isset ($x)) echo "hello";
+    else echo "bay";
+    echo "<br/>";
+    unset ($x);
+    if (isset ($x)) echo "hello";
+    else echo "bay";
+    echo "<br/>";
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
