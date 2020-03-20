901c9251f2b170bd2cc3a51481df3b89dbb1a005
user <user@user.user>
2020-03-20 14:22:45 +0700

    lesson22

---
+++ lesson22.php
@@ -0,0 +1,67 @@
+    <?php
+    $string = "This is example!";
+    echo strlen ($string)."<br/>";
+    echo strpos ($string, "T")."<br/>";
+
+    if (strpos($string, "T") == false) echo "T не найдео";
+    else echo "T не найдено";
+
+    echo "<br/>".substr($string, 3, -2)."<br/>";
+    echo str_replace(array("is", "ple"), array("abc","123"), $string)."<br/>";
+    
+    $str = "<b>ХАХА, жирный шрифт</b>";
+
+    echo htmlspecialchars ($str);
+    echo "<br/>".strtolower($string)."<br/>".strtoupper($string);
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
